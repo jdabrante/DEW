4628c15e916f24a1cad2855191d618c620d278d0
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-10-30 15:51:53 +0000

    Se sube tarea de JS

--- Ut3/a6/js/script.js
+++ Ut3/a6/js/script.js
@@ -1,1 +1,45 @@
 let time = document.getElementById("time");
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let restart = document.getElementById("restart");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let crono;
+
+function start_crono() {
+    if (!crono) {
+        crono = setInterval(calculate, 1000);
+    }
+}
+
+function stop_crono() {
+    if (crono) {
+        clearInterval(crono);
+        crono = null;
+    }
+}
+
+function calculate() {
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    } else if (minutes >= 60) {
+        hours += 1;
+        minutes = 0;
+    } else {
+        seconds += 1;
+    }
+    time.value = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function restart_crono() {
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+}
+
+start.addEventListener("click", start_crono);
+stop.addEventListener("click", stop_crono);
+restart.addEventListener("click", restart_crono);
--- Ut4/a1/js/script.js
+++ Ut4/a1/js/script.js
@@ -1,16 +1,13 @@
 let button = document.getElementById("button");
 
 button.addEventListener("click", function() {
-    let new_window = document.open("new_window.html", "new", "width = 900, height = 900, resizable = false" );
-    let url = new_window.document.location;
-    let protocolo = url.protocol;
-    let navigator_code = new_window.navigator.appCodeName;
+    let new_window = window.open("", "_blank", "width = 900, height = 900, resizable = no" );
     let active_java = new_window.navigator.javaEnabled();
     let java;
     new_window.document.write("<h3>Ejemplo de Ventana Nueva</h3>");
-    new_window.document.write(`<p>URL: ${url}</p>`);
-    new_window.document.write(`<p>Procolo utilizado: ${protocolo}</p>`)
-    new_window.document.write(`<p>Nombre en código del navegador: ${navigator_code}</p>`)
+    new_window.document.write(`<p>URL: ${new_window.location.href}</p>`);
+    new_window.document.write(`<p>Procolo utilizado: ${new_window.location.protocol}</p>`)
+    new_window.document.write(`<p>Nombre en código del navegador: ${new_window.navigator.appCodeName}</p>`)
     if (active_java == true) {
         java = "Java si disponible";
     } else {
@@ -20,30 +17,28 @@ button.addEventListener("click", function() {
     new_window.document.write("<iframe src='www.google.com' width=800 height=600></iframe>")
 })
 
-function data() {
-    let nombre = document.getElementById('nombre').value;
-    let apellidos = document.getElementById('apellidos').value;
-    let fecha_nacimiento = (new Date(document.getElementById('nacimiento').value));
-    let fecha_actual = new Date();
-    let anios = Math.floor((fecha_actual.getTime() - fecha_nacimiento.getTime()) / 31560000000)
-    nombre = String(nombre);
-    let longitud_nombre = nombre.length
-    document.write(`<p>Buenos días ${nombre} ${apellidos}</p>
-    <p>Tu nombre tiene ${longitud_nombre} caracteres, incluidos espacios</p>
-    <p>La primera letra A de tu nombre está en la posición ${nombre.indexOf("a")}</p>
-    <p>La última letra A de tu nombre está en la posición ${nombre.lastIndexOf("a")}</p>
-    <p>Tu nombre menos las 3 primeras letras es "${nombre.slice(3,)}"</p>
-    <p>Tu nombre todo en mayúsculas es "${nombre.toUpperCase()}"</p>
-    <p>Tu edas es: ${anios} años</p>
-    <p>Naciste un feliz ${fecha_nacimiento.getDay()} del ${fecha_nacimiento.getMonth()} del año ${fecha_nacimiento.getFullYear()}
-    <p>El coseno de 180 es ${Math.cos(180)}</p>
-    <p>El número mayor de (34,67,23,7,35,19) es: ${Math.max(34,67,23,7,35,19)}</p>
-    <p>Ejemplo de número al azar ${Math.floor(Math.random() * 100)}</p>`)
-}
 
-document.write(`<h1>TAREA DWEC03</h1>
-<input type="text" placeholder="Nombre" id="nombre"><br>
-<input type="text" placeholder="Apellidos" id="apellidos"><br>
-<input type="date" id="nacimiento"><br>
-<button id="button" onclick="data()">Enviar</button>`)
-
+let nombre = prompt('Nombre y apellidos: ')
+let dia_nacimiento = prompt('Dia de nacimiento: ');
+let mes_nacimiento = prompt('Mes de nacimiento: ');
+let anio_nacimiento = prompt('Año nacimiento: ');
+let fecha_actual = new Date();
+let fecha_nacimiento = new Date(anio_nacimiento, mes_nacimiento, dia_nacimiento);
+let anios = fecha_actual.getFullYear() -  fecha_nacimiento.getFullYear();
+nombre = String(nombre);
+let longitud_nombre = nombre.length
+const dias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+const meses = ['Diciembre','Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre']
+document.write(`
+<h1>TAREA DWEC03</h1>
+<p>Buenos días ${nombre}</p>
+<p>Tu nombre tiene ${longitud_nombre} caracteres, incluidos espacios</p>
+<p>La primera letra A de tu nombre está en la posición ${nombre.toUpperCase().indexOf("A")}</p>
+<p>La última letra A de tu nombre está en la posición ${nombre.toUpperCase().lastIndexOf("A")}</p>
+<p>Tu nombre menos las 3 primeras letras es "${nombre.slice(3,)}"</p>
+<p>Tu nombre todo en mayúsculas es "${nombre.toUpperCase()}"</p>
+<p>Tu edas es: ${anios} años</p>
+<p>El coseno de 180 es ${Math.cos(180)}</p>
+<p>El número mayor de (34,67,23,7,35,19) es: ${Math.max(34,67,23,7,35,19)}</p>
+<p>Ejemplo de número al azar ${Math.floor(Math.random() * 100)}</p>
+<p>Naciste un feliz ${dias[fecha_nacimiento.getDay()]} del ${meses[fecha_nacimiento.getMonth()]} del año ${fecha_nacimiento.getFullYear()}`)
