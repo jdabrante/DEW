d8d5fe33a7984a327ef5b3c141572e3b61891942
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-11-03 18:39:38 +0000

    Se hace le ejer 1

--- Ut5/a1/js/script.js
+++ Ut5/a1/js/script.js
@@ -1,11 +1,17 @@
-let button = document.getElementById("button");
-let number = document.getElementById("number").value;
 
-button.addEventListener("click", function() {
-    let array = new Array()
-    for (let i=0; i <= 10; i++) {
-        if (number == "") {
-            array.push(Math.floor(Math.random() * 200))
-        }
-    }
-})
+
+
+function generate_random_array (number_of_elements=10, min_value=100, max_value=200 ) {
+    let random_array = Array.from({length: number_of_elements}, () => Math.floor(Math.random() * (max_value - min_value) + min_value));
+    document.getElementById("array").innerHTML = random_array.toString();
+};
+
+let button = document.getElementById("generate");
+button.addEventListener("click", generate_random_array());
+
+// let button = document.getElementById('generate');
+// button.addEventListener('click', function () {
+//     let random_array = Array.from({length: 10}, () => Math.floor(Math.random() * (200 - 100) + 100));
+//     document.getElementById("array").innerHTML = random_array.toString();
+// });
+
