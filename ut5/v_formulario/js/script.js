494fcd8b9807bccc5f3e83677ffcebaaf7813175
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-12-04 20:05:43 +0000

    se sube la tarea terminada de validación de formulario

--- ut5/v_formulario/js/script.js
+++ ut5/v_formulario/js/script.js
@@ -1,6 +1,7 @@
 let nombre = document.getElementById("nombre")
 let apellidos = document.getElementById("apellidos")
 
+
 function outFocusUpper(paramenter) {
     paramenter.addEventListener("focusout", () => {
         paramenter.value = paramenter.value.toUpperCase()
@@ -29,7 +30,8 @@ function checkAge() {
 }
 
 function checkDNI() {
-    let re = /\d{8}-[A-Z,a-z]/
+    // Expresión regular para hacer match con una cadena que comience con 8 dígitos, un guión medio "-" y termine con una letra en mayusculas o miníscula
+    let re = /^\d{8}-[A-Z,a-z]$/
     let nif = document.getElementById('nif')
     if (re.exec(nif.value) == null) {
         error.innerHTML = `El campo ${nif.id} es erroneo`
@@ -40,7 +42,9 @@ function checkDNI() {
 }
 
 function checkEmail() {
-    let re = /[a-z,A-Z,0-9]+@{1}[a-z,A-Z]+\.{1}[a-z]+/
+    // Expresión regular para hacer match con una cadena que empiece con uno o mas números o letras, 
+    // una arroba "@", uno o más letras, un punto y  termine con una o más letras minúsculas
+    let re = /^[a-z,A-Z,0-9]+@{1}[a-z,A-Z]+\.{1}[a-z]+$/
     let email = document.getElementById('email')
     if (re.exec(email.value) == null) {
         error.innerHTML = `El campo ${email.id} es erroneo` 
@@ -61,6 +65,8 @@ function checkProvince() {
 }
 
 function checkDate() {
+    // Expresión regular para cadenas de texto que empiecen "^" por cadenas de dos dígitos
+    // y el útlmo de 4 separados por barras "/" o "|" guines medios "-". Terminando en ambos casos por el número de 4 dígitos ( el año)
     let re = /^\d{2}\/\d{2}\/\d{4}$|\d{2}-\d{2}-\d{4}$/
     let date = document.getElementById('fecha')
     if (!re.exec(date.value)) {
@@ -72,6 +78,7 @@ function checkDate() {
 }
 
 function checkPhone() {
+    // Expresión regular que empiece y termina por 9 dígitos
     let re = /^\d{9}$/
     let phone = document.getElementById('telefono')
     if (!re.exec(phone.value)) {
@@ -83,6 +90,7 @@ function checkPhone() {
 }
 
 function checkHour() {
+    // Expresión regular que empiece por dós dígitos, siga con dos puntos ":" y termine con dos dígitos
     let re = /^\d{2}:\d{2}$/
     let hour = document.getElementById('hora')
     if (!re.exec(hour.value)) {
@@ -93,25 +101,20 @@ function checkHour() {
     return true
 }
 
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) {
-    return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    }
-    return null;
-    }
+tries = 0
+intentos = document.getElementById('intentos')
+document.cookie = 'intentos=' + encodeURIComponent(tries)
 
-let tries = 0
-document.cookie = 'tries=' + tries 
-let intentos = document.getElementById('intentos')
+function addTry() {
+    tries += 1
+    document.cookie = 'try=' + encodeURIComponent(tries)
+    // En este punto cre que el reverse no deberia de ser necesario, pero por algún motivo la coockie añadida me sale
+    // como último elemento del array de cookies.
+    let cookie = document.cookie.split(";").reverse()[0].split("=")[1]
+    intentos.innerHTML = `Intento de Evíos del formulario: ${cookie}`
+}
 
 document.getElementById('enviar').addEventListener('click', (event)=>{
-    let cookie;
     if (checkBlankParameter(nombre) && checkBlankParameter(apellidos) && checkAge() && 
     checkDNI() && checkEmail() && checkProvince() && checkDate() && 
     checkDate() && checkPhone() && checkHour()) {
@@ -119,20 +122,15 @@ document.getElementById('enviar').addEventListener('click', (event)=>{
             return true
         } else {
             event.preventDefault()
-            tries += 1
-            cookie = readCookie("tries")
-            intentos.innerHTML = `Intento de Evíos del formulario: ${cookie}`
+            addTry()
             return false
         }
     } else {
         event.preventDefault()
-        tries += 1
-        cookie = readCookie("tries")
-        intentos.innerHTML = `Intento de Evíos del formulario: ${cookie}`
+        addTry()
         return false
     }
 })
-
 
 
 outFocusUpper(nombre)
