0906ce0e0bfb624abe927e833034abc16aa84139
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-01-22 23:59:57 +0000

    Se sube lo hecho en casa

--- UT7/entregable/js/script.js
+++ UT7/entregable/js/script.js
@@ -1,2 +1,59 @@
-const url = "https://www.eldia.es/rss/section/12502"
+
+let noticia = document.getElementById('noticias');
+let createRSS = document.getElementById('crearRSS');
+let deleteRSS = document.getElementById('borrarRSS');
+let choiceRSS = document.getElementById('campoSelect');
+let allContent = {}
+
+createRSS.addEventListener('click', () => {
+    let rssName = prompt('Introduce un titulo para el RSS');
+    let rssUrl = prompt('Introduce la URL');
+    allContent[rssName] = {}
+    fetch(`server.php?url=${rssUrl}`)
+    .then(response => response.json())
+    .then(data => {
+        let channel = data['channel']['item']
+        channel.forEach(element => {
+            allContent[rssName][element['title']] = {'link': element['link'], 'description': element['description']}
+        });
+    });
+    localStorage.setItem(rssName, rssUrl);
+})
+
+
+
+deleteRSS.addEventListener('click', () => {
+    let name = prompt('Introduce el nombre de RSS que desea borrar');
+    localStorage.removeItem(name);
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i=0; i < localStorage.length; i++) {
+        choiceRSS.innerHTML += `<option value='${localStorage.getItem(localStorage.key(i))}'>${localStorage.key(i)}</option>`
+        fetch(`server.php?url=${localStorage.getItem(localStorage.key(i))}`)
+        .then(response => response.json())
+        .then(data => {
+        let channel = data['channel']['item']
+        channel.forEach(element => {
+            allContent[localStorage.key(i)][element['title']] = {'link': element['link'], 'description': element['description']}
+        });
+    });
+    }
+})
+
+choiceRSS.addEventListener('change', () => {
+    let url = choiceRSS.options[choiceRSS.selectedIndex].value
+    noticia.innerHTML = ""
+    fetch(`server.php?url=${url}`)
+    .then(response => response.json())
+    .then(data => {
+        let channel = data['channel']['item']
+        channel.forEach(element => {
+            noticia.innerHTML += `<a href='${element['link']}'><h2>${element['title']}</a></h2><br><p>${element['description']}</p>`
+        });
+    });
+})
+
+
+
 
