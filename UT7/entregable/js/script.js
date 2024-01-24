7b32ab9cb9163f21ac07981791d0b10c2a35d546
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2024-01-24 18:20:08 +0000

    hecho en clase

--- UT7/entregable/js/script.js
+++ UT7/entregable/js/script.js
@@ -8,23 +8,23 @@ createRSS.addEventListener('click', () => {
     let rssUrl = prompt('Introduce la URL');
     localStorage.setItem(rssName, rssUrl);
     noticia.innerHTML = ""
-    choiceRSS.innerHTML += `<option value='${localStorage.getItem(localStorage.key(i))}'>${localStorage.key(i)}</option>`
-    fetch(`server.php?url=${url}`)
-    .then(response => response.json())
-    .then(data => {
-        let channel = data['channel']['item']
-        channel.forEach(element => {
-            noticia.innerHTML += `<a href='${element['link']}'><h2>${element['title']}</a></h2><br><p>${element['description']}</p>`
-        });
-    });
+    choiceRSS.innerHTML += `<option value='${rssUrl}'>${rssName}</option>`
+    asyncPetition(rssUrl)
 })
 
 deleteRSS.addEventListener('click', () => {
     let name = prompt('Introduce el nombre de RSS que desea borrar');
     localStorage.removeItem(name);
+    noticia.innerHTML = ""
+    let firstRss = localStorage.getItem(localStorage.key(0))
+    asyncPetition(firstRss)
+    location.reload()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
+    let firstRss = localStorage.getItem(localStorage.key(0))
+    asyncPetition(firstRss)
+
     for (let i=0; i < localStorage.length; i++) {
         choiceRSS.innerHTML += `<option value='${localStorage.getItem(localStorage.key(i))}'>${localStorage.key(i)}</option>`
     }
@@ -33,6 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
 choiceRSS.addEventListener('change', () => {
     let url = choiceRSS.options[choiceRSS.selectedIndex].value
     noticia.innerHTML = ""
+    asyncPetition(url)
+})
+
+function asyncPetition(url) {
     fetch(`server.php?url=${url}`)
     .then(response => response.json())
     .then(data => {
@@ -41,7 +45,7 @@ choiceRSS.addEventListener('change', () => {
             noticia.innerHTML += `<a href='${element['link']}'><h2>${element['title']}</a></h2><br><p>${element['description']}</p>`
         });
     });
-})
+}
 
 
 
